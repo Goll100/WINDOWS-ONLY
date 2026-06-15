2af86eee22534e9ac563d6fe98b0658ce484e41d
sserunjogisharif48-gif <sserunjogisharif48@gmail.com>
2026-06-15 20:52:09 +0300

    Create Book.cs

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+namespace ScholasticaReader.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+        public string FileType { get; set; }
+    }
+} 
